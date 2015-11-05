97237b0eb8f5ff2bcd8652aa4ea0ed3c08792e08
Chloé Trachsel <chloe.trachsel@IG14-TRACHSELC.intra.eiaj.ch>
2015-11-05 09:56:55 +0100

    Squelette initial

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name moviesApp
+ * @description
+ * # moviesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('moviesApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

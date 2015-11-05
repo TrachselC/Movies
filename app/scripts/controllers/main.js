b7c098fb316a8a27aceabb70d8f6a1285390b9f5
Chloé Trachsel <chloe.trachsel@IG14-TRACHSELC.intra.eiaj.ch>
2015-11-05 12:39:32 +0100

    Squellette prepa

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,31 @@
  */
 angular.module('moviesApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
+    var main = this;
+    main.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    main.messageCool = function(){
+      //ajouter texte à la liste
+      var main = this;
+      main.awesomeThings.push(main.message);
+      //main.message = 'Hello ' + main.message + ' super cool !';
+    };
+
+    main.isMessageVisible = function(){
+      return main.message.length % 2 === 0;
+    };
+
+    main.deleteElement = function(el, array){
+      //supprimer élément de la liste 
+      var ind = array.indexOf(el);
+      array.splice(ind, 1);
+  
+      //slice 
+      //index of //--> index
+    };
+
+    main.message = 'Hello';
   });
